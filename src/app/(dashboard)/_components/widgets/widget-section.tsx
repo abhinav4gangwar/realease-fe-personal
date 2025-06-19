b9c40b171b59528d7f92466ee6f153a12146b1cf
Rio-awsm <mm.saha1982@gmail.com>
2025-06-20 03:35:12 +0530

    feat(dashboard): add RecentActivityWidget and WidgetSection components for activity feed

---
+++ src/app/(dashboard)/_components/widgets/widget-section.tsx
@@ -0,0 +1,11 @@
+import RecentACtivityWidget from "./recent-activity-widget"
+
+const WidgetSection = () => {
+  return (
+    <div>
+      <RecentACtivityWidget />
+    </div>
+  )
+}
+
+export default WidgetSection
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,18 +1,23 @@
-import AddWidgetButton from "./_components/add-widget-menu";
-import QuickActionMenu from "./_components/quick-action-menu";
+import AddWidgetButton from './_components/add-widget-menu'
+import QuickActionMenu from './_components/quick-action-menu'
+import WidgetSection from './_components/widgets/widget-section'
 
 export default function Home() {
   return (
     <div className="">
-      <div className="flex justify-between">
+      <div className="flex justify-between pb-4">
         <div className="text-secondary text-2xl font-semibold lg:text-3xl">
           Dashboard
         </div>
 
         <div className="flex gap-6">
-        <AddWidgetButton />
-         <QuickActionMenu />
+          <AddWidgetButton />
+          <QuickActionMenu />
         </div>
+      </div>
+
+      <div className="py-2">
+        <WidgetSection />
       </div>
     </div>
   )
